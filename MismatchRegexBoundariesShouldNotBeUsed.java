97fe111debe083a0fd489aa445454c5b7f869331
Pooja0509 <80098293+Pooja0509@users.noreply.github.com>
2021-03-05 16:50:10 +0530

    Update MismatchRegexBoundariesShouldNotBeUsed.java

--- MismatchRegexBoundariesShouldNotBeUsed.java
+++ MismatchRegexBoundariesShouldNotBeUsed.java
@@ -4,14 +4,6 @@ package javacodechecker;
 
 import java.util.regex.Pattern;
 
- 
-
- 
-
- 
-
- 
-
 public class MismatchRegexBoundariesShouldNotBeUsed{
     // EMB-ISSUE: CodeIssueNames.MISMATCH_REGEX_BOUNDARIES_SHOULD_NOT_BE_USED
     pattern pattern = Pattern.compile("$[a-z]+^");
