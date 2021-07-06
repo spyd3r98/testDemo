24a36754ad44ddeb7e57dcce3653ccfe81a5d308
Pooja0509 <80098293+Pooja0509@users.noreply.github.com>
2021-07-06 18:37:16 +0530

    Update MapsWithEnumValuesReplaceWithEnumMap.java

--- MapsWithEnumValuesReplaceWithEnumMap.java
+++ MapsWithEnumValuesReplaceWithEnumMap.java
@@ -8,5 +8,23 @@ public class MapsWithEnumValuesReplaceWithEnumMap {
 	Map<Integer,String> map=new HashMap<Integer,String>();      
 	// EMB-ISSUE: CodeIssueNames.MAPS_WITH_ENUM_VALUES_REPLACE_WITH_ENUMMAP
     Map<COLOR, String> moodMap = new HashMap<COLOR, String> ();
+	  Connection conn = null;
+	try {
+		conn = (Connection) DriverManager.getConnection(getUrl());
+	} catch (SQLException e) {
+		// TODO Auto-generated catch block
+		e.printStackTrace();
+	}
+    try {
+    	PreparedStatement  ps = ((java.sql.Connection) conn).prepareStatement("");
+    } catch (SQLException throwables) {
+        new SARDScannerException(throwables);
+    }
+  }
+
+private String getUrl() {
+	// TODO Auto-generated method stub
+	return null;
+}
   }
 }
