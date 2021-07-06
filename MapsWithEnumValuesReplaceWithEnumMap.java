808ae336ca20a29b0259a1f63aba8ab963c719f9
Pooja0509 <80098293+Pooja0509@users.noreply.github.com>
2021-07-06 18:49:01 +0530

    Update MapsWithEnumValuesReplaceWithEnumMap.java

--- MapsWithEnumValuesReplaceWithEnumMap.java
+++ MapsWithEnumValuesReplaceWithEnumMap.java
@@ -1,3 +1,8 @@
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.sql.ResultSet;
+import java.sql.Statement;
 public class MapsWithEnumValuesReplaceWithEnumMap {
 
   public enum COLOR {
@@ -8,23 +13,15 @@ public class MapsWithEnumValuesReplaceWithEnumMap {
 	Map<Integer,String> map=new HashMap<Integer,String>();      
 	// EMB-ISSUE: CodeIssueNames.MAPS_WITH_ENUM_VALUES_REPLACE_WITH_ENUMMAP
     Map<COLOR, String> moodMap = new HashMap<COLOR, String> ();
-	  Connection conn = null;
-	try {
-		conn = (Connection) DriverManager.getConnection(getUrl());
-	} catch (SQLException e) {
-		// TODO Auto-generated catch block
-		e.printStackTrace();
-	}
+	 Connection conn = null;
     try {
-    	PreparedStatement  ps = ((java.sql.Connection) conn).prepareStatement("");
+      String url = "jdbc:sqlite:path-to-db/chinook/chinook.db";
+      conn = DriverManager.getConnection(url);
+
+      System.out.println("Got it!");
+
     } catch (SQLException throwables) {
         new SARDScannerException(throwables);
     }
-  }
-
-private String getUrl() {
-	// TODO Auto-generated method stub
-	return null;
-}
   }
 }
