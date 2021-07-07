eac7b6a9e0b00ae2cfe7604fe2ed3f21d441bada
Pooja0509 <80098293+Pooja0509@users.noreply.github.com>
2021-07-07 13:41:21 +0530

    Update BigIntegerInstantiation.java

--- BigIntegerInstantiation.java
+++ BigIntegerInstantiation.java
@@ -4,7 +4,7 @@ import javax.net.ssl.SSLContext;
 import java.io.*;  
 import java.net.*;  
 import org.apache.http.conn.ssl.SSLSocketFactory;
-class BigIntegerInstantiation{
+class BigIntegerInstantiation throws IllegalArgumentException{
 	public static void main(String args[]){  
 		// EMB-ISSUE: CodeIssueNames.BIG_INTEGER_INSTANTIATION
         BigInteger bigInteger = new BigInteger("1");  
@@ -19,10 +19,23 @@ class BigIntegerInstantiation{
             bigInteger = bigInteger.multiply(BigInteger.valueOf(i));  
         }  
         System.out.println("Factorial of 4 : "+bigInteger);  
-    	
+    	try {
+			addInteger(1);
+		} catch (IllegalArgumentException iae) {
+			iae.printStackTrace();
+		}
 	}
 	     SSLContext context1 = SSLContext.getInstance("SSL");
 	     SSLContext context = SSLContext.getInstance("TLS");
 	     Socket soc = new Socket("www.google.com",80);
 	     Socket soc1 = SSLSocketFactory.getDefault().createSocket("www.google.com", 443);
+	private static List<Integer> integers = new ArrayList<Integer>();
+public static void addInteger(Integer value) throws IllegalArgumentException {
+		if (integers.contains(value)) {
+			 new IllegalArgumentException("Integer already added.");
+		}
+		integers.add(value);
+	}
+	
 }
+
